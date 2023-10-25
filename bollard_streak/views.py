ab89ab5414cb9210eeedf863090f222febfb7e2e
joshkabloomy <joshkabloomy1220@gmail.com>
2023-10-25 13:54:39 -0400

    fixed streak sync

--- bollard_streak/views.py
+++ bollard_streak/views.py
@@ -4,7 +4,9 @@ import random
 from .forms import CheckCountry
 from django.http import HttpResponse
 
+prev = ""
 def bollard_streak(request):
+    global prev
     table_size = CountryStreak.objects.last().pk
     r = random.randint(1, table_size) 
     data = CountryStreak.objects.get(pk=r)
@@ -12,20 +14,22 @@ def bollard_streak(request):
 
     #check_country(request)
     correct = False
+    country = ""
     if request.method == 'POST':
+        print("send help")
         form = CheckCountry(request.POST)
         if form.is_valid():
             word_to_check = form.cleaned_data['word_to_check']
-            print(word_to_check + "  " + data.countries)
-            if word_to_check == data.countries:
-                print("Correct")
+            print(word_to_check + "  " + prev.countries)
+            if word_to_check == prev.countries:
+                country = word_to_check
                 correct = True
             else:
                 correct = False
     else:
         form = CheckCountry()
-
-    return render(request, 'bollard_streak/country_streak.html', {'data': data, 'form': form, 'correct': correct})
+    prev = data
+    return render(request, 'bollard_streak/country_streak.html', {'data': data, 'form': form, 'correct': correct, 'country': country})
 
 # def check_country(request):
 #     print(request.method)
