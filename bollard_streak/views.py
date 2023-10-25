a2691765ee9b2bd429ac68907e7df28b4cdd4135
joshkabloomy <joshkabloomy1220@gmail.com>
2023-10-25 14:50:39 -0400

    added and fixed incorrect html page

--- bollard_streak/views.py
+++ bollard_streak/views.py
@@ -5,7 +5,9 @@ from .forms import CheckCountry
 from django.http import HttpResponse
 
 prev = ""
+counter = 0
 def bollard_streak(request):
+    global counter
     global prev
     table_size = CountryStreak.objects.last().pk
     r = random.randint(1, table_size) 
@@ -21,15 +23,21 @@ def bollard_streak(request):
         if form.is_valid():
             word_to_check = form.cleaned_data['word_to_check']
             print(word_to_check + "  " + prev.countries)
-            if word_to_check == prev.countries:
-                country = word_to_check
+            country = prev.countries
+            lower_case_country = country.lower() #lowercase input
+            arr = lower_case_country.split(",")
+            if word_to_check.lower() in arr:
+                counter += 1
                 correct = True
             else:
+                endstreak = counter
+                counter = 0
                 correct = False
+                return render(request, 'bollard_streak/incorrect.html', {'data': prev, 'form': form, 'correct': correct, 'country': country, 'counter': endstreak})
     else:
         form = CheckCountry()
     prev = data
-    return render(request, 'bollard_streak/country_streak.html', {'data': data, 'form': form, 'correct': correct, 'country': country})
+    return render(request, 'bollard_streak/country_streak.html', {'data': data, 'form': form, 'correct': correct, 'country': country, 'counter': counter})
 
 # def check_country(request):
 #     print(request.method)
