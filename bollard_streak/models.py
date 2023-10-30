aca73a8aa304b8f0ce98e7eae20c36c8529a33c5
joshkabloomy <joshkabloomy1220@gmail.com>
2023-10-29 22:19:42 -0400

    added sessions

--- bollard_streak/models.py
+++ bollard_streak/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class CountryStreak(models.Model):
     countries = models.CharField(max_length=200)
     image_link = models.CharField(max_length=200)
-    class Meta: # define what the board name is
+    class Meta: # define what the model name is
         db_table = 'country_table'
--- bollard_streak/views.py
+++ bollard_streak/views.py
@@ -4,56 +4,55 @@ import random
 from .forms import CheckCountry
 from django.http import HttpResponse
 
-prev = ""
-counter = 0
+#prev = ""
+#counter = 0
 def bollard_streak(request):
-    global counter
-    global prev
+    #global counter
+    #global prev
+
+    # Session
+    counter = request.session.get('counter', 0)
+    if 'prev_pk' in request.session:
+        prev_pk = request.session['prev_pk']
+        prev = CountryStreak.objects.get(pk=prev_pk)
+    else:
+        prev = None
+
+
+    # Fetch image from sql database
     table_size = CountryStreak.objects.last().pk
     r = random.randint(1, table_size) 
     data = CountryStreak.objects.get(pk=r)
     print(data.countries)
-
-    #check_country(request)
+    
     correct = False
     country = ""
     if request.method == 'POST':
-        print("send help")
+        print(request.POST)
         form = CheckCountry(request.POST)
         if form.is_valid():
             word_to_check = form.cleaned_data['word_to_check']
-            print(word_to_check + "  " + prev.countries)
+            print(word_to_check + " | " + prev.countries)
             country = prev.countries
             lower_case_country = country.lower() #lowercase input
             arr = lower_case_country.split(",")
+            arr = [x.strip(" ") for x in arr]
             if word_to_check.lower() in arr:
                 counter += 1
+                request.session['counter'] = counter
                 correct = True
             else:
                 endstreak = counter
-                counter = 0
+                request.session['counter'] = 0
                 correct = False
                 return render(request, 'bollard_streak/incorrect.html', {'data': prev, 'form': form, 'correct': correct, 'country': country, 'counter': endstreak})
     else:
         form = CheckCountry()
-    prev = data
+    
+    request.session['prev_pk'] = r # saves the last primary key
     return render(request, 'bollard_streak/country_streak.html', {'data': data, 'form': form, 'correct': correct, 'country': country, 'counter': counter})
 
-# def check_country(request):
-#     print(request.method)
-#     if request.method == 'POST':
-#         form = CheckCountry(request.POST)
-#         if form.is_valid():
-#             word_to_check = form.cleaned_data['word_to_check']
-#             if word_to_check == data.countries:
-#                 print("Correct")
-#                 return HttpResponse("Word matches the value.")
-#             else:
-#                 return HttpResponse("Word does not match the value.")
-#     else:
-#         form = CheckCountry()
 
-#     return render(request, 'bollard_streak/country_streak.html', {'form': form})
 
 
 
